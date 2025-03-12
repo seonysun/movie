d1f2064e1ab56b67e442cff9cd5158a4efd17432
seonysun <csh99240@naver.com>
2025-03-12 11:20:32 +0900

    style: boolean 조건 수정

--- src/components/common/ToggleButton.jsx
+++ src/components/common/ToggleButton.jsx
@@ -4,7 +4,7 @@ import useTheme from "../../hooks/useTheme";
 function ToggleButton() {
   const { theme, toggleTheme } = useTheme();
 
-  const [isOn, setIsOn] = useState(theme === "dark" ? true : false);
+  const [isOn, setIsOn] = useState(theme === "dark");
 
   useEffect(() => {
     document.documentElement.classList[theme === "dark" ? "add" : "remove"](
