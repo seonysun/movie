b259d3889fa5b3ff98be39871574aae6915b468a
seonysun <csh99240@naver.com>
2025-03-17 15:36:01 +0900

    refactor: 초기 렌더링 시 ssr에서도 에러 발생하지 않도록 초기 실행 과정 추가

--- src/hooks/useResize.js
+++ src/hooks/useResize.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import useDebouncedFn from "./useDebouncedFn";
 
 const useResize = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(true);
 
   const handleResize = useDebouncedFn(() => {
     setIsMobile(window.innerWidth < 768);
   }, 300);
 
   useEffect(() => {
+    setIsMobile(window.innerWidth < 768);
+
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, [handleResize]);
